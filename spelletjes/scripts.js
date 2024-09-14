e60a7e822a3733eb16ada67a578ce9d3895ac4b4
Sjoerd3345 <sjoertrret@gmail.com>
2024-09-14 22:12:31 +0200

    Update scripts.js

--- spelletjes/scripts.js
+++ spelletjes/scripts.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 gameFrame.requestFullscreen();
             } else if (gameFrame.mozRequestFullScreen) { // Firefox
                 gameFrame.mozRequestFullScreen();
-            } else if (gameFrame.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            } else if (gameFrame.webkitRequestFullscreen) { // Chrome, Safari, and Opera
                 gameFrame.webkitRequestFullscreen();
             } else if (gameFrame.msRequestFullscreen) { // IE/Edge
                 gameFrame.msRequestFullscreen();
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.exitFullscreen();
             } else if (document.mozCancelFullScreen) { // Firefox
                 document.mozCancelFullScreen();
-            } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+            } else if (document.webkitExitFullscreen) { // Chrome, Safari, and Opera
                 document.webkitExitFullscreen();
             } else if (document.msExitFullscreen) { // IE/Edge
                 document.msExitFullscreen();
